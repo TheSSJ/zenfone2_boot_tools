0f09b0fc1e2085575d39bbf59fc531f260039afe
TheSSJ <bla@bla.de>
2015-06-20 10:29:52 -0700

    fixed malformed bootheader

--- bootheader.h
+++ bootheader.h
@@ -7,23 +7,28 @@
 #ifndef __BOOTHEADER__
 #define __BOOTHEADER__
 
-#define HEAD_PADDING						(0x40) /* Common Android Intel Image, correct */
-
-#define CMDLINE_SIZE   						(0x200) //correct
+#define HEAD_PADDING						(0x20)
+#define MAGIC							(0x8) //ANDROID!
+#define CMDLINE_SIZE   						(0x200)
 #define PADDING1_SIZE						(0x800-0x254) //5AC
-#define BOOTSTUBSTACK_SIZE					(0x0) //there seems to be none
-#define CMDLINE_END						(HEAD_PADDING+CMDLINE_SIZE)
+#define CMDLINE_END						(HEAD_PADDING+HEAD_PADDING+CMDLINE_SIZE) //super dirty workaround
 
 struct bootheader {
-	char padding0[HEAD_PADDING]; //0x40
-	char cmdline[CMDLINE_SIZE]; //0x200
-	uint32_t bzImageSize; //4 bytes
-	uint32_t initrdSize; //4
-	uint32_t SPIUARTSuppression; //4
-	uint32_t SPIType; //4
+	char magicword[MAGIC]; //8 Bytes
+	uint32_t bzImageSize; //4 Bytes
+	uint32_t bzImageOffset; //4 Bytes
+	uint32_t initrdSize; //4 
+	uint32_t initrdOffset; //4
 	uint32_t secondSize; //4
+	uint32_t secondOffset; //4
+	char padding0[HEAD_PADDING]; //0x20
+	char cmdline[CMDLINE_SIZE]; //0x200
+	uint32_t id1; //4
+	uint32_t id2; //4
+	uint32_t id3; //4
+	uint32_t id4; //4
+	uint32_t id5; //4
 	char padding1[PADDING1_SIZE]; //5AC
-	//char bootstubStack[BOOTSTUBSTACK_SIZE];
 };
 
 /* Sanity check for struct size */
